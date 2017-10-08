5d4b8cd5a827a46a6a78cf934ad0b0bdae412725
nivi.mor <nivi.mor@experitest.com>
2017-10-08 12:52:54 +0300

    fixed ios and android tests

--- android.rb
+++ android.rb
@@ -5,17 +5,16 @@ class TestYourAndroidApp < Test::Unit::TestCase
  def setup
      desired_caps = {
          caps: {
-		accessKey: '<ACCESS_KEY>',
-		platformName: 'android',
-		testName: '<TEST_NAME>'
-		projectName: '<YOUR_PROJECT_NAME>',
-		app: 'cloud:<BUNDLE_ID>',
-		appPackage: '<BUNDLE_ID>',
-		appActivity: '<ACTIVITY>' 
-         },
-		appium_lib: {	
-			server_url: 'https://cloud.experitest.com:443/wd/hub',
-		}
+            accessKey: '<ACCESS_KEY>',
+            platformName: 'android',
+            testName: '<TEST_NAME>',
+            app: 'cloud:<BUNDLE_ID>',
+            appPackage: '<BUNDLE_ID>',
+            appActivity: '<ACTIVITY>'
+                 },
+            appium_lib: {
+              server_url: 'https://cloud.experitest.com:443/wd/hub',
+            }
      }
      @driver = Appium::Driver.new(desired_caps)
      @driver.start_driver
--- ios.rb
+++ ios.rb
@@ -5,16 +5,15 @@ class TestYouriOSApp < Test::Unit::TestCase
  def setup
      desired_caps = {
          caps: {
-		accessKey: '<ACCESS_KEY>',
-		platformName: 'ios',
-		projectName: '<YOUR_PROJECT_NAME>',
-		testName: '<TEST_NAME>'
-		app: 'cloud:<BUNDLE_ID>',
-		bundleId: '<BUNDLE_ID>',
-         },
-	 	appium_lib: {
-			server_url: 'https://cloud.experitest.com:443/wd/hub',
-		}
+            accessKey: '<ACCESS_KEY>',
+            platformName: 'ios',
+            testName: '<TEST_NAME>',
+            app: 'cloud:<BUNDLE_ID>',
+            bundleId: '<BUNDLE_ID>',
+                 },
+            appium_lib: {
+              server_url: 'https://cloud.experitest.com:443/wd/hub',
+		        }
      }
      @driver = Appium::Driver.new(desired_caps)
      @driver.start_driver
